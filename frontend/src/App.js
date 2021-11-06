bd1c9e0cb0cd1a27062b1bbb1d2eb94283f3625a
Ali Sadri <asadri721@gmail.com>
2021-11-06 16:11:22 +0330

    Create reset password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Entry from './pages/Entry';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
     <div className='App'>
       <Entry />
+      <ResetPassword />
     </div>
   );
 }
---
+++ frontend/src/pages/ResetPassword.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Container, Row, Col, Form, Card, Button } from 'react-bootstrap';
+import './entry.css';
+
+const Entry = () => {
+  const [email, setEmail] = useState('');
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setEmail(value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      return alert('Please enter the email'); //TODO Use Toast or sweetalert
+      //TODO Call api to submit the form
+    }
+  };
+
+  return (
+    <div className='entry-page bg-info'>
+      <Container>
+        <Row>
+          <Col className='form-login'>
+            <Card className='form-login-card'>
+              <Card.Body>
+                <Card.Title className='text-center'>
+                  <h2>Reset Password</h2>
+                </Card.Title>
+                <Form onSubmit={submitHandler}>
+                  <Form.Group className='my-3'>
+                    <Form.Control
+                      valuue={email}
+                      onChange={onChangeHandler}
+                      type='email'
+                      name='email'
+                      placeholder='Email'
+                    />
+                  </Form.Group>
+                  <Button type='submit' className='my-3'>
+                    Reset Password
+                  </Button>
+                </Form>
+                <Card.Text>
+                  <a href='#'>Login Again?</a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Entry;
